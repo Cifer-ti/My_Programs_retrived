540b2c51665c4a3081cad20373aa543c2caa9d0c
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-06-11 19:20:52 +0100

    a script to output system information of a computer on a web page

--- shell_script/_0_/sys_info_page.sh
+++ shell_script/_0_/sys_info_page.sh
@@ -20,3 +20,133 @@ report_uptime () {
 	return
 }
 
+#outputs the amount of disk free
+report_disk_space () {
+	cat <<- _EOF_
+
+		<h2>Disk Space Utilization</h2>
+		<pre>$(df -h)</pre>
+
+		_EOF_
+	return
+}
+
+#outputs the amount of space used by home directory
+report_home_space () {
+	if [[ $(id -u) -eq 0 ]]; then
+		cat <<- _EOF_
+
+			<h2>Home Space Utilization (All Users)</h2>
+			<pre>$(du -sh /home/*)</pre>
+
+			_EOF_
+
+	else
+
+		cat <<- _EOF_
+
+			<h2>Home Space Utilization ($USER)</h2>
+			<pre>$(du -sh $HOME)</pre>
+
+			_EOF_
+
+	fi
+	return
+}
+
+usage () {
+	echo "$PROGNAME: usage: $PROGNAME [-f filename | -i]]"
+	return
+}
+
+write_html_page () {
+	cat <<- _EOF_
+	
+	<html>
+
+		<head>
+			<title>$TITLE</title>
+		</head>
+
+		<body>
+
+			<h1>$TITLE</h1>
+			<p>$TIME_STAMP</p>
+			$(report_uptime)
+			$(report_disk_space)
+			$(report_home_space)
+
+		</body>
+
+	<html>
+	_EOF_
+	return
+}
+
+# process command line options
+
+interactive=
+filename=
+
+while [[ -n $1 ]]; do
+	case $1 in
+
+		-f | --file)		shift
+					filename=$1
+					;;
+
+		-i | --interactive)	interactive=1
+					;;
+
+		-h | --help)		usage
+					exit
+					;;
+
+		*)			usage >&2
+					exit 1
+					;;
+					
+	esac
+	shift
+done
+
+#interactive mode
+
+if [[ -n $interactive ]]; then
+	while true; do
+		read -p "Enter name of output file: " filename
+
+		if [[ -e $filename ]]; then
+			read -p "'$filename' exists. Overwrite ? [y/n/q] >"
+
+			case $REPLY in
+				Y | y)	break
+					;;
+
+				q | Q) echo "Program terminaed."
+					exit
+					;;
+
+				*)	continue
+					;;
+
+			esac
+		fi
+	done
+fi
+
+#output html page
+
+if [[ -n $filename ]]; then
+	if touch $filename && [[ -f $filename ]]; then
+		write_html_page > $filename
+
+	else
+		echo "$PRGNAME: cannot write file '$filename'" >&2
+		exit 1
+		
+	fi
+
+else
+	write_html_page
+fi
