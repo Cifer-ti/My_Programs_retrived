fca795851b7061535a13d1782ef73611f0c9938f
Cifer-ti <draculemihawk232@gmail.com>
2023-12-04 17:39:10 +0100

    Add Long option
    An argp-enabled program, with a ling option, that has a mendatory
    arguement

---
+++ c_files/0_Extra_resource/argp/step3/step3.c
@@ -0,0 +1,30 @@
+/* An argp-enabled program, with a long option, that has a mendatory arguement */
+#include <argp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int parse_opt(int key, char *arg, struct argp_state *state)
+{
+    switch(key) {
+        case 'd':
+            unsigned int i;
+
+            for(i = 0; i < atoi(arg); i++)
+                printf(".\n");
+                break;
+    }
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    struct argp_option options[] =
+    {
+        {"dot", 'd', "NUM", 0, "Show some dots on the screen"},
+        {0}
+    };
+
+    struct argp argp = {options, parse_opt};
+
+    return argp_parse(&argp, argc, argv, 0, 0, 0);
+}
