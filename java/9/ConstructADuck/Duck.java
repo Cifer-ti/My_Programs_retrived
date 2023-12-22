ad13bb95beeff7844a814332b2741a20a2334c33
Cifer-ti <draculemihawk232@gmail.com>
2023-12-22 20:39:49 +0100

    Adding an arguemnet to the constructor

--- java/9/ConstructADuck/Duck.java
+++ java/9/ConstructADuck/Duck.java
@@ -1,8 +1,12 @@
 package ConstructADuck;
 
 public class Duck {
+		int size;
 
-		public Duck() {
+		public Duck(int ducksizie) {
 				System.out.println("Quack");
+
+				size = ducksizie;
+				System.out.println("Size is : " + size);
 		}
 }
--- java/9/ConstructADuck/UseADuck.java
+++ java/9/ConstructADuck/UseADuck.java
@@ -3,6 +3,6 @@ package ConstructADuck;
 public class UseADuck {
 		
 		public static void main(String[] args) {
-				Duck d = new Duck();
+				Duck d = new Duck(43);
 		}
 }
