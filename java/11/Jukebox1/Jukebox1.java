438396dfcf996e8d85330c625124d70d265b4ac4
Cifer-ti <draculemihawk232@gmail.com>
2023-12-24 23:28:41 +0100

    Sort a list
    *Jukebox1:
    Using Collection.sort(), to sort a list in alphabetical order.

--- java/11/Jukebox1/Jukebox1.java
+++ java/11/Jukebox1/Jukebox1.java
@@ -1,32 +1,30 @@
 import java.util.*;
 
-
 public class Jukebox1 {
-		public static void main(String[] args) {
-				new Jukebox1().go();
-		}
+	public static void main(String[] atgs) {
+		new Jukebox1().go();
+	}
 
-		public void go() {
-				List<String> songList = MockSongs.getSongStrings();
-				System.out.println(songList);
-				Collections.sort(songList);
-				System.out.println(songList);
-		}
+	public void go() {
+		List<String> songList = MockSongs.getSongStrings();
+		System.out.println(songList);
+		Collections.sort(songList);
+		System.out.println(songList);
+	}
 }
-
-
 
 class MockSongs {
-		public static List<String> getSongStrings() { // ArrayList is a List
-				List<String> songs = new ArrayList<>();
+	public static List<String> getSongStrings() { // ArrayList is a List
+		List<String> songs = new ArrayList<>();
 
-				songs.add("somersault");
-				songs.add("cassidy");
-				songs.add("$10");
-				songs.add("havana");
-				songs.add("Cassydy");
-				songs.add("50 Ways");
+		songs.add("somersault");
+		songs.add("cassidy");
+		songs.add("$10");
+		songs.add("havana");
+		songs.add("Cassydy");
+		songs.add("50 Ways");
 
-				return songs;
-		}
+		return songs;
+	}
 }
+		
