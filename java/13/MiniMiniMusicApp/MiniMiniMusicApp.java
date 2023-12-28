76e060c8f4fce768c5eeedbc484e81c266dafbb8
Cifer-ti <draculemihawk232@gmail.com>
2023-12-28 21:14:44 +0100

    Add MiniMiniMusicApp class for MIDI playback
    [MiniMiniMusicApp]

---
+++ java/13/MiniMiniMusicApp/MiniMiniMusicApp.java
@@ -0,0 +1,36 @@
+// play a single note on a piano
+
+import javax.sound.midi.*;
+import static javax.sound.midi.ShortMessage.*;
+
+public class MiniMiniMusicApp {
+	public static void main(String[] args) {
+		MiniMiniMusicApp mt = new MiniMiniMusicApp();
+		mt.play();
+	}
+	public void play() {
+		try {
+			Sequencer player = MidiSystem.getSequencer();
+			player.open();
+
+			Sequence seq = new Sequence(Sequence.PPQ, 4);
+
+			Track track = seq.createTrack();
+
+			ShortMessage msg1 = new ShortMessage();
+			msg1.setMessage(144, 1, 44, 100);
+			MidiEvent noteOn = new MidiEvent(msg1, 1);
+			track.add(noteOn);
+
+			ShortMessage msg2 = new ShortMessage();
+			msg2.setMessage(128, 1, 44, 100);
+			MidiEvent noteOff = new MidiEvent(msg2, 16);
+			track.add(noteOff);
+
+			player.setSequence(seq);
+			player.start();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
