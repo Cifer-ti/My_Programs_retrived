711f60a50998d245517ec4a092e1730795a1beb8
Cifer-ti <draculemihawk232@gmail.com>
2024-04-05 16:52:56 +0100

    minor changes

--- java/JavaFx/HelloFx/src/main/java/org/example/hellofx/HelloApplication.java
+++ java/JavaFx/HelloFx/src/main/java/org/example/hellofx/HelloApplication.java
@@ -4,8 +4,15 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
@@ -18,17 +25,61 @@ public class HelloApplication extends Application {
   public void start(Stage stage) throws Exception {
 
     Group root = new Group();
-    Scene scene = new Scene(root, Color.BLACK);
+    Scene scene = new Scene(root, 600, 600, Color.AZURE);
     Image icon = new Image("lookDownSu.jpg");
 
-    stage.getIcons().add(icon);
-    stage.setTitle("Void Of H00d");
-    stage.setWidth(420);
-    stage.setHeight(420);
-    stage.setResizable(false);
-    stage.setFullScreen(true);
-    stage.setFullScreenExitHint("YOU CAN'T EXIT UNLESS YOU... ");
-    stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+    Text text = new Text();
+    text.setText("Void of H00d");
+    text.setX(50);
+    text.setY(50);
+    text.setFont(Font.font("Verdana", 50));
+    text.setFill(Color.LIME);
+
+    Line line = new Line();
+    line.setStartX(200);
+    line.setStartY(200);
+    line.setEndX(500);
+    line.setEndY(200);
+    line.setStrokeWidth(5);
+    line.setStroke(Color.RED);
+    line.setOpacity(0.4);
+    line.setRotate(45);
+
+    Rectangle rectangle = new Rectangle();
+    rectangle.setX(100);
+    rectangle.setY(100);
+    rectangle.setWidth(100);
+    rectangle.setHeight(100);
+    rectangle.setFill(Color.SILVER);
+    rectangle.setStrokeWidth(5);
+    rectangle.setStroke(Color.BLACK);
+
+    Polygon triangle = new Polygon();
+    triangle.getPoints().setAll(
+            200.0, 200.0,
+            300.0, 300.0,
+            200.0, 300.0
+          );
+
+    triangle.setFill(Color.YELLOW);
+
+    Circle circle = new Circle();
+    circle.setCenterX(350);
+    circle.setCenterY(350);
+    circle.setRadius(50);
+    circle.setFill(Color.ORANGE);
+
+    //Image image = new Image("lookDownSu.jpg ");
+    //ImageView imageView = new ImageView(image);
+    //imageView.setX(400);
+    //imageView.setY(400);
+
+    root.getChildren().add(text);
+    root.getChildren().add(line);
+    root.getChildren().add(rectangle);
+    root.getChildren().add(triangle);
+    root.getChildren().add(circle);
+   // root.getChildren().add(imageView);
 
     stage.setScene(scene);
     stage.show();
