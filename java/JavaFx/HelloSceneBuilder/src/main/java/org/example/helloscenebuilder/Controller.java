09c489e28510d7a39059a57404dc4b0e3f70d531
Cifer-ti <draculemihawk232@gmail.com>
2024-04-06 01:08:27 +0100

    some changes I don't really know

--- java/JavaFx/HelloSceneBuilder/src/main/java/org/example/helloscenebuilder/Controller.java
+++ java/JavaFx/HelloSceneBuilder/src/main/java/org/example/helloscenebuilder/Controller.java
@@ -1,14 +1,30 @@
 package org.example.helloscenebuilder;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
 
-public class HelloController {
-  @FXML
-  private Label welcomeText;
+public class Controller {
 
   @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
+  private Circle myCircle;
+  private double x;
+  private double y;
+
+  public void up(ActionEvent e) {
+    //System.out.println("UP");
+    myCircle.setCenterY(y-=7);
+  }
+  public void down(ActionEvent e) {
+    //System.out.println("DOWN");
+    myCircle.setCenterY(y+=7);
+  }
+  public void left(ActionEvent e) {
+    //System.out.println("LEFT");
+    myCircle.setCenterX(x-=7);
+  }
+  public void right(ActionEvent e) {
+    //System.out.println("RIGHT");
+    myCircle.setCenterX(x+=7);
   }
 }
